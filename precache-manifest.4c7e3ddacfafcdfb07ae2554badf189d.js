c5e95ed8284c3412fa266025d56449b4b664da46
Sriram <46366477+ram170@users.noreply.github.com>
2020-08-21 16:40:35 +0530

    Updates

---
+++ precache-manifest.4c7e3ddacfafcdfb07ae2554badf189d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7388da09857de954bbe3eb6a31fbcda",
+    "url": "/random/index.html"
+  },
+  {
+    "revision": "d199b8685ffd42a29c28",
+    "url": "/random/static/css/main.be90e799.chunk.css"
+  },
+  {
+    "revision": "b6acc78b48c26a67ef54",
+    "url": "/random/static/js/2.91b6ce22.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/random/static/js/2.91b6ce22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d199b8685ffd42a29c28",
+    "url": "/random/static/js/main.3e137a88.chunk.js"
+  },
+  {
+    "revision": "62fe7c7749dfc67a9de6",
+    "url": "/random/static/js/runtime-main.c5a1bf72.js"
+  },
+  {
+    "revision": "05885835c004fb6fd354880c850e6472",
+    "url": "/random/static/media/Yrif.05885835.gif"
+  },
+  {
+    "revision": "c611f17a091626c25d509c381d7b0c6f",
+    "url": "/random/static/media/astro2.c611f17a.gif"
+  },
+  {
+    "revision": "b616732224e1863945bb4fd74da7861b",
+    "url": "/random/static/media/itachiUchiha.b6167322.gif"
+  },
+  {
+    "revision": "2dbed312ca6168659bcde19be184fd15",
+    "url": "/random/static/media/shootingstar.2dbed312.gif"
+  },
+  {
+    "revision": "d0e9759945c861cdeee0847ace6f6c0a",
+    "url": "/random/static/media/sunandstars.d0e97599.gif"
+  },
+  {
+    "revision": "3040de9df8ec7b7e47f9103a87e9ede3",
+    "url": "/random/static/media/tree.3040de9d.gif"
+  }
+]);
